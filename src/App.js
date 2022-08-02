30141af153414d51127468daeb9e05898c910067
Sashank Rampalli <sashankr97@gmail.com>
2022-08-02 12:23:34 +0530

    updated console messages

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
     this.state = {
       monsters: [],
     };
-    console.log(1);
+    console.log("constructor");
   }
 
   componentDidMount() {
-    console.log(3);
+    console.log("componentDidMount");
 
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -31,7 +31,7 @@ class App extends Component {
   }
 
   render() {
-    console.log(2);
+    console.log("render");
 
     return (
       <div className="App">
