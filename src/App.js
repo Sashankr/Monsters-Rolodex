d0925f12e4ab6ebee0fa8538b33254600d6c5488
Sashank Rampalli <sashankr97@gmail.com>
2022-08-02 13:04:29 +0530

    filter data based on search

--- src/App.js
+++ src/App.js
@@ -35,6 +35,16 @@ class App extends Component {
 
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.includes(event.target.value);
+            });
+            console.log(filteredMonsters);
+          }}
+        />
         {this.state.monsters.map((data) => {
           return <h1 key={data.id}>{data.name}</h1>;
         })}
