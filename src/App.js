4edbd774aa92fb640e878a827a5e975a7be0e3d2
Sashank Rampalli <sashankr97@gmail.com>
2022-08-18 11:23:30 +0530

    search box component moved to separate component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 class App extends Component {
@@ -44,12 +45,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={onSearchChange}
+        <SearchBox
+          onChangeHandler={onSearchChange}
+          placeholder={"search monsters"}
+          className={"search-box"}
         />
-
         <CardList monsters={filteredMonsters} />
       </div>
     );
