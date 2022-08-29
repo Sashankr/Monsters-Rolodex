97b2ba5827b80b804f0067a89db89fab52c29a03
Sashank Rampalli <sashankr97@gmail.com>
2022-08-29 12:00:59 +0530

    Performance optimization with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-import { Component } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 import { useState } from "react";
 import { useEffect } from "react";
 
-// class App extends Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       monsters: [],
-//       searchField: "",
-//     };
-//   }
-
-//   onSearchChange = (event) => {
-//     const searchField = event.target.value.toLowerCase();
-//     this.setState(() => {
-//       return { searchField };
-//     });
-//   };
-
-//   render() {
-//     const { monsters, searchField } = this.state;
-//     const { onSearchChange } = this;
-
-// const filteredMonsters = monsters.filter((monster) => {
-//   return monster.name.toLowerCase().includes(searchField);
-// });
-
-//     return (
-//       <div className="App">
-//         <h1 className="app-title">Monsters Rolodex</h1>
-//         <SearchBox
-//           onChangeHandler={onSearchChange}
-//           placeholder={"search monsters"}
-//           className={"monsters-search-box"}
-//         />
-//         <CardList monsters={filteredMonsters} />
-//       </div>
-//     );
-//   }
-// }
-
 const App = () => {
-  debugger;
-  console.log("render");
-
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
@@ -67,7 +24,6 @@ const App = () => {
     });
 
     setFilteredMonsters(newFilteredMonsters);
-    console.log(newFilteredMonsters);
   }, [searchField, monsters]);
 
   const onSearchChange = (event) => {
