4794a7af08389aa143e00b6d478b6e603d1598f1
Sashank Rampalli <sashankr97@gmail.com>
2022-08-29 12:00:21 +0530

    useEffect for external resources and side effects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 import { useState } from "react";
+import { useEffect } from "react";
 
 // class App extends Component {
 //   constructor() {
@@ -44,29 +45,45 @@ import { useState } from "react";
 // }
 
 const App = () => {
+  debugger;
   console.log("render");
 
   const [searchField, setSearchField] = useState("");
   const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [count, setCount] = useState(0);
 
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLowerCase().includes(searchField);
-  });
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setMonsters(users);
+      });
+  }, []);
 
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((users) => {
-      setMonsters(users);
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
+
+    setFilteredMonsters(newFilteredMonsters);
+    console.log(newFilteredMonsters);
+  }, [searchField, monsters]);
 
   const onSearchChange = (event) => {
     const searchFieldText = event.target.value.toLowerCase();
     setSearchField(searchFieldText);
   };
 
+  const incrementCount = () => {
+    setCount((count) => count + 1);
+  };
+
   return (
     <div>
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">
+        Monsters Rolodex <span onClick={incrementCount}>{count}</span>{" "}
+      </h1>
       <SearchBox
         onChangeHandler={onSearchChange}
         placeholder={"search monsters"}
